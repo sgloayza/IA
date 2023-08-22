998f5f307e2170510d99802be02f377e6fd857d1
sgloayza <sgloayza@espol.edu.ec>
2023-08-22 08:31:39 -0500

    requests

--- src/app/estudiante/estudiante.component.ts
+++ src/app/estudiante/estudiante.component.ts
@@ -2,8 +2,6 @@ import { Component, OnInit, ViewChild  } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 import { MatTableDataSource } from '@angular/material/table';
 import { HttpClient } from '@angular/common/http';
-import { catchError } from 'rxjs/operators';
-import { of } from 'rxjs';
 import { MatPaginator } from '@angular/material/paginator';
 
 interface StationData {
@@ -33,12 +31,37 @@ export class EstudianteComponent implements OnInit  {
     
     this.dataSource.paginator = this.paginator;
 
+
+    /*    
     //PRUEBA GET
-    fetch("https://bike-loans-server-pr-1.onrender.com/")
+    fetch("http://localhost:5000/")
       .then(resultado => resultado.json())
       .then(resultado => {
-        console.log(resultado)
+        console.log("AAAAA",resultado)
     })
+
+    //PRUEBA POST
+    const requestData = {
+      "station_ids": [
+        1,
+        2
+      ],
+      "start_datetime": "2023-05-17 15:00:00",
+      "end_datetime": "2023-05-19 13:00:00"
+    };
+    
+    console.log(requestData);
+    
+    fetch('http://localhost:5000/predict', {
+      method: 'POST',
+      body: JSON.stringify(requestData), // Convertir a JSON string
+      mode: 'no-cors'
+    }).then((response) => {
+      console.log("BBBB",response);
+    }).catch(error => {
+      console.log("CCCC",error)
+    })
+    */
   }
 
   dateValidation(formGroup: FormGroup) {
@@ -54,89 +77,69 @@ export class EstudianteComponent implements OnInit  {
     return null;
   }
 
+  formatDate(dateTime:Date) {
+    const date = new Date(dateTime);
+  
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+  
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+  }
+
   onSubmit() {
     if (this.dateTimeForm.valid) {
       const formData = this.dateTimeForm.value;
       console.log('Datos del formulario:', formData);
-
-
-/*
-      // Simulación de datos del endpoint
-      const dataFromEndpoint: { stations: StationData } = {
-        stations: {
-          1: {
-            "2021-06-21 07:00:00": 0,
-            "2021-06-21 08:00:00": 1,
-            "2021-06-21 09:00:00": 3,
-          },
-          2: {
-            "2021-06-21 07:00:00": 0,
-            "2021-06-21 08:00:00": 1,
-            "2021-06-21 09:00:00": 3,
-          },
-        }
+      
+      const requestData = {
+        "station_ids": [
+          1,
+          2
+        ],
+        "start_datetime": "2023-05-17 15:00:00", //this.formatDate(formData.datetimeInicio),
+        "end_datetime": "2023-05-19 13:00:00" //this.formatDate(formData.datetimeFin)
       };
+      
+      console.log(requestData," AAAAAAAAAAA");
+      
+      fetch('http://localhost:5000/predict', {
+        method: 'POST',
+        body: JSON.stringify(requestData),
+        headers: {
+          'Content-Type': 'application/json' // Asegurarse de establecer el encabezado correcto
+        },
+        mode: 'cors' // Cambiar a modo cors para acceder a la respuesta
+      })
+      .then(response => response.json())
+      .then(dataFromServer => {
+        console.log("Respuesta del servidor:", dataFromServer);
 
-      // Transformar los datos en un formato compatible con MatTableDataSource
-      const transformedData = [];
-      for (const station in dataFromEndpoint.stations) {
-        for (const date in dataFromEndpoint.stations[station]) {
+        // Transformar los datos en el formato adecuado para la tabla
+        const transformedData = [];
+
+        //solo estacion SELECCIONADA
+        const stationId = parseInt(formData.numeroEstacion); // Estación SELECCIONADA
+        const stationData = dataFromServer.stations[stationId];
+        
+        for (const date in stationData) {
           transformedData.push({
-            numeroEstacion: +station, // Convertir a número
-            fecha: date,
-            numeroEntero: dataFromEndpoint.stations[+station][date] // Convertir a número
+            numeroEstacion: stationId,
+            fecha: new Date(date),
+            numeroEntero: stationData[date]
           });
         }
-      }
 
-      this.dataSource.data = transformedData;
-      */
+        // Asignar los datos a la fuente de datos de la tabla
+        this.dataSource.data = transformedData;
+      })
+      .catch(error => {
+        console.log("Error:", error);
+      });
 
-
-
-      /*
-      // Llenar la matriz dataSource con los datos del formulario
-      const newRow = {
-        numeroEstacion: formData.numeroEstacion,
-        fecha: formData.datetimeInicio, // O datetimeFin, según corresponda
-        numeroEntero: 0 // Inicialmente, puedes establecer este valor en 0
-      };
-
-      this.dataSource.data.push(newRow);
-      this.dataSource.data = [...this.dataSource.data]; // Actualizar la referencia
-      */
-
-      const requestData = {
-        station_ids: [formData.numeroEstacion],
-        start_datetime: formData.datetimeInicio,
-        end_datetime: formData.datetimeFin
-      };
-  
-      this.http.post<any>('https://bike-loans-server-pr-1.onrender.com/predict', requestData)
-        .pipe(
-          catchError(error => {
-            console.error('Error en la solicitud POST:', error);
-            return of(null); // Devuelve un observable con valor nulo para que el flujo continúe
-          })
-        )
-        .subscribe(response => {
-          if (response) {
-            console.log('Respuesta del servidor:', response);
-  
-            /*
-            // Agregar la respuesta a la tabla si es necesario
-            // Por ejemplo, asumiendo que el servidor devuelve un número entero:
-            const newRow = {
-              numeroEstacion: formData.numeroEstacion,
-              fecha: formData.datetimeInicio,
-              numeroEntero: response.prediction
-            };
-  
-            this.dataSource.data.push(newRow);
-            this.dataSource.data = [...this.dataSource.data]; // Actualizar la referencia
-            */
-          }
-        });
     } else {
       console.log('Formulario inválido. Por favor, complete los campos correctamente.');
     }
--- src/app/staff/staff.component.ts
+++ src/app/staff/staff.component.ts
@@ -2,8 +2,6 @@ import { Component, OnInit, ViewChild  } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 import { MatTableDataSource } from '@angular/material/table';
 import { HttpClient } from '@angular/common/http';
-import { catchError } from 'rxjs/operators';
-import { of } from 'rxjs';
 import { MatPaginator } from '@angular/material/paginator';
 
 interface StationData {
@@ -32,13 +30,44 @@ export class StaffComponent implements OnInit {
     
     this.dataSource.paginator = this.paginator;
 
+    /*    
     //PRUEBA GET
-    fetch("https://bike-loans-server-pr-1.onrender.com/")
+    fetch("http://localhost:5000/")
       .then(resultado => resultado.json())
       .then(resultado => {
-        console.log(resultado)
+        console.log("AAAAA",resultado)
     })
+
+    //PRUEBA POST
+    const requestData = {
+      "station_ids": [
+        1,
+        2
+      ],
+      "start_datetime": "2023-05-17 15:00:00",
+      "end_datetime": "2023-05-19 13:00:00"
+    };
+    
+    console.log(requestData);
+    
+    fetch('http://localhost:5000/predict', {
+      method: 'POST',
+      body: JSON.stringify(requestData), // Convertir a JSON string
+      mode: 'no-cors'
+    }).then((response) => {
+      console.log("BBBB",response);
+    }).catch(error => {
+      console.log("CCCC",error)
+    })
+    */
   }
+  
+
+
+
+
+
+  
 
   dateValidation(formGroup: FormGroup) {
     const datetimeInicio = formGroup.get('datetimeInicio')?.value;
@@ -53,10 +82,68 @@ export class StaffComponent implements OnInit {
     return null;
   }
 
+  formatDate(dateTime:Date) {
+    const date = new Date(dateTime);
+  
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+  
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+  }
+
   onSubmit() {
     if (this.dateTimeForm.valid) {
       const formData = this.dateTimeForm.value;
       console.log('Datos del formulario:', formData);
+      
+      const requestData = {
+        "station_ids": [
+          1,
+          2
+        ],
+        "start_datetime": "2023-05-17 15:00:00", //this.formatDate(formData.datetimeInicio),
+        "end_datetime": "2023-05-19 13:00:00" //this.formatDate(formData.datetimeFin)
+      };
+      
+      console.log(requestData," AAAAAAAAAAA");
+      
+      fetch('http://localhost:5000/predict', {
+        method: 'POST',
+        body: JSON.stringify(requestData),
+        headers: {
+          'Content-Type': 'application/json' // Asegurarse de establecer el encabezado correcto
+        },
+        mode: 'cors' // Cambiar a modo cors para acceder a la respuesta
+      })
+      .then(response => response.json())
+      .then(dataFromServer => {
+        console.log("Respuesta del servidor:", dataFromServer);
+
+        // Transformar los datos en el formato adecuado para la tabla
+        const transformedData = [];
+
+        //AMBAS estaciones
+        for (const stationId in dataFromServer.stations) {
+          for (const date in dataFromServer.stations[stationId]) {
+            transformedData.push({
+              numeroEstacion: parseInt(stationId, 10),
+              fecha: new Date(date),
+              numeroEntero: dataFromServer.stations[stationId][date]
+            });
+          }
+        }
+
+        // Asignar los datos a la fuente de datos de la tabla
+        this.dataSource.data = transformedData;
+      })
+      .catch(error => {
+        console.log("Error:", error);
+      });
+
     } else {
       console.log('Formulario inválido. Por favor, complete los campos correctamente.');
     }
